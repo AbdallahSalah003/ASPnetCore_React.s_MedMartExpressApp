d418525bbcdbc83fbed35005e5dd5a373a07639b
AbdallahSalah <abdallahsalahsalem9@gmail.com>
2023-11-27 20:36:25 +0200

    Adding User and Admin Components

---
+++ MedMartExpressApp/ClientApp/components/RouterPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+export default function RouterPage() {
+    return (
+        <div>Router</div>
+    )
+}
---
+++ MedMartExpressApp/ClientApp/components/Users/MedicineDisplay.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export default function MedicineDisplay() {
+    return (
+        <div>MedicineDisplay</div>
+    )
+} 
